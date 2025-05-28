baf3d4b9c625024662bbf28d5a1e65c42e221b9e
kogut <tomakoz456@gmail.com>
2025-05-28 20:16:07 +0200

    working scanning multiple directory and generating html file

--- kpics.py
+++ kpics.py
@@ -30,30 +30,25 @@ def md5(str):
 def scan_dir(_photos_dir_path):
     _photos = []
     _photo_list = {}
+    print(_photos_dir_path)
     try:
     # if not os.path.isdir(_photos_dir_path):
-        
-        print("Star scanning photos directory in: %s" % _photos_dir_path)
-        for root, sub_folder, files in os.walk(_photos_dir_path):
-            
-            if '_files' not in root:
-
-                print("root: %s" % root)
-                
-                # if len(sub_folder) > 0:
-                #     for folder in sub_folder:
-                #         folder_full_path = os.path.join(root, folder)
-                #         print("fullpath: %s" % folder_full_path)
-                
-                if len(files) > 0:
-                    for _file in files:
-                        _file_name = _file.endswith(_file)
-                        _extension = pathlib.Path(_file).suffix
-                        if(_extension in ('.jpg', '.jpeg', '.png')):
-                            _full_path = os.path.join(root, _file)
-                            print("Full path: %s" % _full_path)
-                            _photos.append(_full_path)
-                            _photo_list[len(_photo_list)] = _full_path
+        if type(_photos_dir_path) is not dict:
+            _photos_dir_path = [_photos_dir_path]
+        for _photos_dir in _photos_dir_path.values():
+            print("Star scanning photos directory in: %s" % _photos_dir)
+            for root, sub_folder, files in os.walk(_photos_dir):        
+                if '_files' not in root:
+                    print("root: %s" % root)
+                    if len(files) > 0:
+                        for _file in files:
+                            _file_name = _file.endswith(_file)
+                            _extension = pathlib.Path(_file).suffix
+                            if(_extension in ('.jpg', '.jpeg', '.png')):
+                                _full_path = os.path.join(root, _file)
+                                print("Full path: %s" % _full_path)
+                                _photos.append(_full_path)
+                                _photo_list[len(_photo_list)] = _full_path
 
         return _photo_list
     except IOError:
@@ -66,65 +61,61 @@ def thumb(_photo_path):
     _thumb_dir = r'C:\Users\kogut\Documents\.kpics'
     
 
+class KpicsProcessor:
+    def __init__(self, photos_dir_path, thumb_dir, thumb_height=153):
+        # if type(photos_dir_path) is not dict:
+        #     raise ValueError("photos_dir_path must be a dictionary with photo dir IDs as keys and paths as values.")
+        self.photos_dir_path = photos_dir_path
+        self.thumb_dir = thumb_dir
+        self.thumb_height = thumb_height
+        self.photos = scan_dir(self.photos_dir_path)
+        self.thumb_dir_name = hashlib.md5(list(self.photos_dir_path.values())[0].encode()).hexdigest()
+        self.thumb_dir_path = os.path.join(self.thumb_dir, self.thumb_dir_name)
+        self.html = ''
+        self.ensure_thumb_dir()
+
+    def ensure_thumb_dir(self):
+        if not os.path.isdir(self.thumb_dir_path):
+            os.mkdir(self.thumb_dir_path)
+            print("Create dir for thumb files in: %s" % self.thumb_dir_path)
+
+    def process_photos(self):
+        id = 0
+        for photo_id in self.photos:
+            path = self.photos[photo_id]
+            thumb_name = "%s.%s" % (hashlib.md5(path.encode()).hexdigest(), path.split('.')[-1])
+            thumb_full_path = os.path.join(self.thumb_dir_path, thumb_name)
+            if os.path.isfile(path):
+                if not os.path.isfile(thumb_full_path):
+                    photo = cv2.imread(path)
+                    if photo is None:
+                        print("Ignoring wrong photo file: %s" % path)
+                        continue
+                    print("photo shape for: %s" % path)
+                    photo_height, photo_width = photo.shape[:2]
+                    thumb_height = self.thumb_height
+                    thumb_width = int((thumb_height * photo_width) / photo_height)
+                    thumb = cv2.resize(photo, (int(thumb_width), int(thumb_height)), thumb_width/photo_width, thumb_height/photo_height)
+                    cv2.imwrite(thumb_full_path, thumb)
+                else:
+                    print("Thumb file already exists: %s" % thumb_full_path)
+                    thumb = cv2.imread(thumb_full_path)
+                    thumb_height, thumb_width = thumb.shape[:2]
+                self.html += '<a id="photo-' + str(id) + '" href="' + path + '" class="thumb" title="' + os.path.basename(path) + ' in ' + os.path.dirname(path) + '">'
+                self.html += '<img src="' + thumb_full_path + '" alt="'+ os.path.basename(thumb_full_path) + '" width="'+ str(thumb_width) +'" height="'+ str(thumb_height) + '"/>'
+                self.html += '</a>'
+                id += 1
 
 if __name__ == '__main__':
-    _photos_dir_path = r'K:\trainman\fb'
-    # _photos_dir_path = r'C:\Users\kogut\Desktop\piec'
-    _thumb_dir = r'C:\Users\kogut\Documents\.kpics'
-    _thumb_height = 153
-    _photos = scan_dir(_photos_dir_path)
-    # print("md5: %s is a %s" % (hashlib.md5("%s" % _photos[len(_photos)-1].encode()).hexdigest(), _photos[len(_photos)-1]))
-    # _path = _photos[len(_photos)-1]
-    # print(_path)
-    _html = ''
-    _thumb_dir_name = hashlib.md5(_photos_dir_path.encode()).hexdigest()
-    _thumb_dir_path = os.path.join(_thumb_dir, _thumb_dir_name)
-    id = 0
-    if not os.path.isdir(_thumb_dir_path):
-        os.mkdir(_thumb_dir_path)
-        print("Create dir for thumb files in: %s" % _thumb_dir_path)
-
-    for _photo_id in _photos:
-        # if _photo_id is 10:
-            # break
-        _path = _photos[_photo_id]
-        _thumb_name = "%s.%s" % (hashlib.md5(_path.encode()).hexdigest(), _path.split('.')[-1])
-        # _thumb_path = os.path.join(_thumb_dir_path, _thumb_name)
-        _thumb_full_path = os.path.join(_thumb_dir_path, _thumb_name)
-        if os.path.isfile(_path): # and not os.path.isfile(_thumb_full_path):
-            if not os.path.isfile(_thumb_full_path): # check if 
-                photo = cv2.imread(_path)
-                if photo is None:
-                    print("Ignoring wrong photo file: %s" % _path)
-                    continue
-                print("photo shape for: %s" % _path)
-                photo_height, photo_width = photo.shape[:2]
-
-                # proportion = photo_width / photo_height
-                thumb_height = _thumb_height
-
-                # scale = photo_height / thumb_height
-                # thumb_width = int(photo_width / scale) 
-                # if photo_height > photo_width:
-                #     thumb_width = photo_width / scale
-                # elif photo_height < photo_width:
-                #     thumb_width = photo_width / scale
-                # else:
-                #     thumb_width = photo_width / scale
-                thumb_width = int((thumb_height * photo_width) / photo_height)
-                # if (thumb_width / thumb_height) is not proportion:
-                    # print("wrong photo dimmension for file: %s => %s" % (_path, proportion))
-                _thumb = cv2.resize(photo, (int(thumb_width), int(thumb_height)), thumb_width/photo_width, thumb_height/photo_height)
-                cv2.imwrite(_thumb_full_path, _thumb)
-            else:
-                print("Thumb file already exists: %s" % _thumb_full_path)
-                thumb = cv2.imread(_thumb_full_path)
-                thumb_height, thumb_width = thumb.shape[:2]
-            _html += '<a id="photo-' + str(id) + '" href="' + _path + '" class="thumb" title="' + os.path.basename(_path) + ' in ' + os.path.dirname(_path) + '">'
-            _html += '<img src="' + _thumb_full_path + '" alt="'+ os.path.basename(_thumb_full_path) + '" width="'+ str(thumb_width) +'" height="'+ str(thumb_height) + '"/>'
-            _html += '</a>'
-            id += 1
-            # print(_thumb_full_path)
+    photos_dir_dict = {0: r'K:\trainman\fb'}
+    processor = KpicsProcessor(
+        photos_dir_path=photos_dir_dict,
+        thumb_dir=r'C:\Users\kogut\Documents\.kpics',
+        thumb_height=153
+    )
+    processor.process_photos()
+    _html = processor.html
+    _thumb_dir_path = processor.thumb_dir_path
 
 _html_name = 'index.html'
 _html_path = os.path.join(_thumb_dir_path, _html_name)
